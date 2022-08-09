47fa09e0e90f78758fcf840d7e63561ed4fad270
RahulNeupane <nrahul503@gmail.com>
2022-08-09 08:32:07 +0545

    updated

--- js/script.js
+++ js/script.js
@@ -3,33 +3,25 @@
 
 
 $('.feedback-slider').slick({
-    dots: true,
-    infinite: false,
-    speed: 300,
-    slidesToShow: 4,
-    slidesToScroll: 4,
+    dots: false,
+    infinite: true,
+    speed: 1000,
+    mobileFirst:true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    prevArrow: '<button class="slick-prev" aria-label="Previous" type="button"><i class="icon-prev"></i></button>',
+    nextArrow: '<button class="slick-next" aria-label="Next" type="button"><i class="icon-next"></i></button>',
     responsive: [
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 2,
+        }
+      },
       {
         breakpoint: 1024,
         settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
-          dots: true
-        }
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
           slidesToShow: 1,
-          slidesToScroll: 1
         }
       }
       // You can unslick at a given breakpoint now by adding:
